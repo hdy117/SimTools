2d5d3f948a170fc07abac2d69198ff5433d9cc85
dongyuanhu <dongyuanhu@tencent.com>
2023-12-23 11:37:16 +0800

    remove zmq deps, use vcpkg

--- cppzmq_playground/message/generate_cpp.sh
+++ cppzmq_playground/message/generate_cpp.sh
@@ -6,6 +6,3 @@ for f in $root_dir/*.proto
 do
   protoc -I=$root_dir --cpp_out=$root_dir $f
 done
-
-GRPC_CPP_PLUGIN=/opt/grpc/bin/grpc_cpp_plugin
-/opt/grpc/bin/protoc -I=$root_dir --grpc_out=$root_dir --plugin=protoc-gen-grpc=$GRPC_CPP_PLUGIN $root_dir/HelloService.proto
