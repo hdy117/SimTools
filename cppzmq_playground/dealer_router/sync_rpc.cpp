074ad9e07591fbe2e640eb35fbf5fb84d20d86ae
hudongyuan <hudongyuan@zhidaoauto.com>
2023-12-25 19:25:08 +0800

    adding sync rpc

--- cppzmq_playground/dealer_router/sync_rpc.cpp
+++ cppzmq_playground/dealer_router/sync_rpc.cpp
@@ -1,2 +1,155 @@
 #include "sync_rpc.h"
+#include "sim_log.h"
 
+void MessageHelper::printMessage(const std::string& topic, const std::string& payload) {
+	if (topic::LOCATION == topic) {
+		sim::Location location;
+		location.ParseFromString(payload);
+		MessageHelper::printMessage(topic, location);
+	}
+	else if (topic::TRAJECTORY == topic) {
+		sim::Trajectory trajectory;
+		trajectory.ParseFromString(payload);
+		MessageHelper::printMessage(topic, trajectory);
+	}
+	else {
+		LOG_ERROR << "unknown topic " << topic << "\n";
+	}
+}
+
+void MessageHelper::printMessage(const std::string& topic, const google::protobuf::Message& message) {
+	LOG_0 << "topic:" << topic << ", payload:" << message.DebugString() << "\n";
+}
+
+bool MessageHelper::parseFromMessage(const zmq::message_t& msg, google::protobuf::Message& message) {
+	message.ParseFromArray(msg.data(), msg.size() - 1);
+	return true;
+}
+
+/*============================================================*/
+
+Client::Client(const std::string& serverIP, const std::string& port, const std::string& clientID) {
+	serverIP_ = serverIP;
+	serverPort_ = port;
+	id_ = clientID;
+	serverAddr_ = "tcp://" + serverIP_ + ":" + serverPort_;
+	LOG_0 << "serverAddr_:" << serverAddr_ << "\n";
+
+	context_ = zmq::context_t(1);
+	rpcSocket_ = zmq::socket_t(context_, zmq::socket_type::dealer);
+	if (id_.empty()) {
+		LOG_ERROR << "client id should be unique, current is " << clientID << "\n";
+		throw std::runtime_error("client id should be unique(not empty)");
+	}
+	rpcSocket_.setsockopt(ZMQ_IDENTITY, id_.c_str(), id_.size() + 1);
+	try {
+		rpcSocket_.connect(serverAddr_.c_str());
+		LOG_0 << "client id is " << id_ << "\n";
+	}
+	catch (const std::exception& e) {
+		internalState_.setState(1, "fail to connect server");
+		LOG_ERROR << "error:" << e.what() << "\n";
+		throw std::runtime_error("fail to connect server");
+	}
+}
+Client::~Client() {
+	rpcSocket_.close();
+	context_.close();
+}
+sim::RPCServiceStatus Client::getMessageByTopic(const std::string& topic, std::string& payload) {
+	// clear firstly
+	payload.clear();
+
+	sim::RPCServiceStatus serviceStatus;
+	if (topic.empty()) {
+		serviceStatus.set_state(1);
+		serviceStatus.set_info("empty topic will not be served");
+		return serviceStatus;
+	}
+
+	// rpc call info
+	sim::RPCCallInfo callInfo;
+	callInfo.set_funcname("getMessageByTopic_string_string");
+	callInfo.set_flag(0);
+	std::string callInfoPayload;
+	callInfo.SerializeToString(&callInfoPayload);
+
+	// prepare msg to send
+	zmq::message_t callInfoMsg(callInfoPayload.size() + 1), topicMsg(topic.size() + 1);
+
+	// send rpc call
+	rpcSocket_.send(callInfoMsg, zmq::send_flags::sndmore);
+	rpcSocket_.send(topicMsg, zmq::send_flags::none);
+
+	// recv rpc response
+	zmq::message_t servStateMsg;
+	rpcSocket_.recv(servStateMsg, zmq::recv_flags::none);
+	MessageHelper::parseFromMessage(servStateMsg, serviceStatus);
+
+	// if serv is fatal
+	if (serviceStatus.state() == FATAL_STATE) {
+		return serviceStatus;
+	}
+		
+	// recv
+	zmq::message_t replyMsg;
+	rpcSocket_.recv(replyMsg, zmq::recv_flags::none);
+	
+	// copy data out
+	MessageHelper::parseFromMessage(servStateMsg, serviceStatus);
+	
+	// post work
+	MessageHelper::printMessage(topic, payload);
+	
+	return serviceStatus;
+}
+
+sim::RPCServiceStatus Client::setMessageByTopic(const std::string& topic, const std::string& payload) {
+	sim::RPCServiceStatus serviceStatus;
+	if (topic.empty()) {
+		serviceStatus.set_state(1);
+		serviceStatus.set_info("empty topic will not be served");
+		return serviceStatus;
+	}
+
+	// rpc call info
+	sim::RPCCallInfo callInfo;
+	callInfo.set_funcname("setMessageByTopic_string_string");
+	callInfo.set_flag(0);
+	std::string callInfoPayload;
+	callInfo.SerializeToString(&callInfoPayload);
+
+	// prepare msg to send
+	sim::MsgPair pair;
+	pair.set_topic(topic.c_str());
+	pair.set_payload(payload.c_str(), pair.size());
+	std::string payloadPair;
+	pair.SerializeToString(&payloadPair);
+	zmq::message_t callInfoMsg(callInfoPayload.size() + 1), pairMsg(payloadPair.size() + 1);
+
+	// send rpc call
+	rpcSocket_.send(callInfoMsg, zmq::send_flags::sndmore);
+	rpcSocket_.send(pairMsg, zmq::send_flags::none);
+
+	// recv rpc response
+	zmq::message_t servStateMsg;
+	rpcSocket_.recv(servStateMsg, zmq::recv_flags::none);
+	MessageHelper::parseFromMessage(servStateMsg, serviceStatus);
+
+	// if serv is fatal
+	if (serviceStatus.state() == FATAL_STATE) {
+		return serviceStatus;
+	}
+
+	// recv
+	zmq::message_t replyMsg;
+	rpcSocket_.recv(replyMsg, zmq::recv_flags::none);
+
+	// copy data out
+	MessageHelper::parseFromMessage(servStateMsg, serviceStatus);
+
+	// post work
+	MessageHelper::printMessage(topic, payload);
+
+	return serviceStatus;
+}
--- cppzmq_playground/dealer_router/sync_rpc.h
+++ cppzmq_playground/dealer_router/sync_rpc.h
@@ -5,11 +5,114 @@
 #include "message/location.pb.h"
 #include "message/basic.pb.h"
 #include "message/trajectory.pb.h"
+#include "message/rpc.pb.h"
 #include "zmq.hpp"
 
 #include <string>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <future>
+#include <functional>
+#include <unordered_map>
+
+#define FATAL_STATE 2
 
 namespace topic {
 const std::string LOCATION("LOCATION");
 const std::string TRAJECTORY("TRAJECTORY");
 } // namespace topic
+
+class InternalState {
+public:
+	InternalState():state_(0), info_("OK") {}
+	virtual ~InternalState() {}
+	void setState(uint32_t state, const std::string& info="OK") {
+		std::lock_guard<std::mutex> guard(lock_);
+		state_ = state;
+		info_ = info;
+	}
+	uint32_t getState() {
+		std::lock_guard<std::mutex> guard(lock_);
+		return state_;
+	}
+	std::string getStateInfo() {
+		std::lock_guard<std::mutex> guard(lock_);
+		return 	info_;
+		;
+	}
+private:
+	uint32_t state_;
+	std::string info_;
+	std::mutex lock_;
+};
+
+class MessageHelper {
+public:
+	static void printMessage(const std::string& topic, const std::string& payload);
+	static void printMessage(const std::string& topic, const google::protobuf::Message& message);
+	static bool parseFromMessage(const zmq::message_t& msg, google::protobuf::Message& message);
+};
+
+/**
+ * @brief sync_rpc client, 
+ * send data format: id, RPCCallInfo, request_payload(protobuf or string, service should know how to deal)
+ * reply data format: id, RPCCallInfo, RPCServiceStatus(if 2, no reply), reply_payload(protobuf or string, client should know how to deal) 
+*/
+class Client {
+public:
+	Client(const std::string& serverIP, const std::string& port, const std::string& clientID);
+	virtual ~Client();
+
+	Client(const Client&) = delete;
+	Client& operator=(const Client&) = delete;
+public:
+	//sim::RPCServiceStatus registerWithServer();
+	sim::RPCServiceStatus getMessageByTopic(const std::string& topic, std::string& payload);
+	sim::RPCServiceStatus setMessageByTopic(const std::string& topic, const std::string& payload);
+private:
+	//sim::RPCServiceStatus sendHeartBeat();
+private:
+	std::string id_;
+	std::string serverIP_, serverPort_, serverAddr_;
+	std::atomic<uint8_t> stop_;
+	InternalState internalState_;			// state of client, 0 means ok, 1 means warning, 2 means fatal
+
+	zmq::context_t context_;
+	zmq::socket_t rpcSocket_;
+};
+
+// service 
+class ServiceImp;
+using ServiceImpPtr = std::shared_ptr<ServiceImp>;
+
+class Server {
+public:
+	Server(const std::string& serverIP, const std::string& port, const std::string& clientID);
+	virtual ~Server();
+
+	Server(const Server&) = delete;
+	Server& operator=(const Server&) = delete;
+public:
+	void serve();
+protected:
+	void serveGetMessageByTopic();
+private:
+	ServiceImpPtr service_;
+	std::string serverIP_, serverPort_, serverAddr_;
+	zmq::context_t context_;
+};
+
+class ServiceImp {
+public:
+	ServiceImp();
+	virtual ~ServiceImp();
+
+	ServiceImp(const ServiceImp&) = delete;
+	ServiceImp& operator=(const ServiceImp&) = delete;
+
+	sim::RPCServiceStatus getMessageByTopic(const std::string& topic, std::string& payload);
+	sim::RPCServiceStatus setMessageByTopic(const std::string& topic, const std::string& payload);
+private:
+	
+};
